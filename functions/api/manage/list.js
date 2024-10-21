1ff185b4680112137b37e80fca13ed754bdac34c
浪小矢 <djchan411130@gmail.com>
2024-10-21 17:01:05 +0800

    Update list.js

--- functions/api/manage/list.js
+++ functions/api/manage/list.js
@@ -8,19 +8,22 @@ export async function onRequest(context) {
       next, // used for middleware or to fetch assets
       data, // arbitrary space for passing data between middlewares
     } = context;
-    console.log(value)
-    //let res=[]
-    //for (let i in value.keys){
-      //add to res
-      //"metadata":{"TimeStamp":19876541,"ListType":"None","rating_label":"None"}
-      //let tmp = {
-      //  name: value.keys[i].name,
-      //  TimeStamp: value.keys[i].metadata.TimeStamp,
-      //  ListType: value.keys[i].metadata.ListType,
-      //  rating_label: value.keys[i].metadata.rating_label,
-      //}
-      //res.push(tmp)
-    //}
+    console.log(env)
+    const value = await env.img_url.list();
+    console.log("value: ", value);
+
+    let res=[];
+    for (let i in value.keys){
+      // add to res
+      // "metadata":{"TimeStamp":19876541,"ListType":"None","rating_label":"None"}
+      let tmp = {
+       name: value.keys[i].name,
+       TimeStamp: value.keys[i].metadata.TimeStamp,
+       ListType: value.keys[i].metadata.ListType,
+       rating_label: value.keys[i].metadata.rating_label,
+      }
+      res.push(tmp)
+    }
     const info = JSON.stringify(value.keys);
     return new Response(info);
 
