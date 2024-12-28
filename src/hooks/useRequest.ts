b2a33182b191d5d94feb1cbc6d65dfa1e4982c23
txwdzxq <txwdzxq@sina.com>
2024-12-29 00:59:49 +0800

    use

--- src/hooks/useRequest.ts
+++ src/hooks/useRequest.ts
@@ -1,13 +1,15 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.BASE_URL
+const BASIC_USER = import.meta.env.BASIC_USER
+const BASIC_PASS = import.meta.env.BASIC_PASS
 
 export default function useRequest() {
   const upload = async (files: Array<File>) => {
     if (files) {
       console.log('upload' + files)
       try {
-        console.log(BASE_URL)
+        console.log('BASIC_USER:' + BASIC_USER)
+        console.log('BASIC_USER:' + BASIC_PASS)
         const result = await axios.post('http://localhost:8080/upload', files);
         console.log(result)
       } catch (e) {
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:'./',
   plugins: [
     vue(),
     vueDevTools(),
