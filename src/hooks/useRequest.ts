cb1f8b43a98c833e83adb16e0c23dba627abf4b8
txwdzxq <txwdzxq@sina.com>
2024-12-29 02:21:19 +0800

    use

--- src/hooks/useRequest.ts
+++ src/hooks/useRequest.ts
@@ -1,23 +1,33 @@
 import axios from "axios";
+import {type PagesFunction, type KVNamespace, Response as CfResponse} from "@cloudflare/workers-types";
+
+interface Env {
+    KV: KVNamespace;
+}
 
 
+export function useRequest() {
+    const upload = async (files: Array<File>) => {
+        if (files) {
+            console.log('upload' + files)
+            try {
+                const result = await axios.post('http://localhost:8080/upload', files);
+                console.log(result)
+            } catch (e) {
+                console.log(e)
+            }
+        }
+    };
 
-export default function useRequest(context: any) {
-  const upload = async (files: Array<File>) => {
-    if (files) {
-      console.log('upload' + files)
-      try {
-        console.log('BASIC_USER:' + context.env.BASIC_USER)
-        console.log('BASIC_USER:' + context.env.BASIC_PASS)
-        const result = await axios.post('http://localhost:8080/upload', files);
-        console.log(result)
-      } catch (e) {
-        console.log(e)
-      }
+    return {
+        upload
     }
-  };
+}
 
-  return {
-    upload
-  }
+
+export async function onRequest(context: any) {
+    console.log(context.env.BASIC_USER)
+    console.log(context.env.BASIC_PASS)
+
+    return new Response();
 }
