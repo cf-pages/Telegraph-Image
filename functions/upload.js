f55086f0b97f037e88ade0314103e8cc25d9f48b
txwdzxq <txwdzxq@sina.com>
2024-12-29 10:12:15 +0800

    3

--- functions/upload.js
+++ functions/upload.js
@@ -1,4 +1,4 @@
 export function onRequest(context) {
   console.log(context)
-  return new Response("Hello, world!"+context.env.BASE_USER)
+  return new Response("Hello, world!"+context.env.BASIC_USER)
 }
