9437cbc7a29d249e410ff998fa730bfef2b81178
txwdzxq <txwdzxq@sina.com>
2024-12-30 00:45:12 +0800

    is

--- functions/file/[path].ts
+++ functions/file/[path].ts
@@ -8,9 +8,9 @@ export const onRequest: PagesFunction<Env> = async (context) => {
   const {request, env,} = context;
   // get file path json
   const path = request.url.split(".")[0].split("/")[2];
-  // const url = `https://api.telegram.org/bot${env['TG_Bot_Token']}/getFile?file_id=${path}`;
+  const url = `https://api.telegram.org/bot${env['TG_Bot_Token']}/getFile?file_id=${path}`;
   console.log(path);
-  const url = `https://api.telegram.org/bot8162291976:AAGznGhSV8Pj47IfoEGj6UyQJWKtrB191qg/getFile?file_id=AgACAgUAAyEGAASQv4IqAAMHZ29c4WtA7PjpKtLaCcKvVEDAEjAAAkPBMRssGXhXRD_Nin8nX64BAAMCAAN5AAM2BA`;
+  // const url = `https://api.telegram.org/bot8162291976:AAGznGhSV8Pj47IfoEGj6UyQJWKtrB191qg/getFile?file_id=AgACAgUAAyEGAASQv4IqAAMHZ29c4WtA7PjpKtLaCcKvVEDAEjAAAkPBMRssGXhXRD_Nin8nX64BAAMCAAN5AAM2BA`;
   let file_path;
   axios.get(url).then(
     (res: AxiosResponse) => {
@@ -20,16 +20,19 @@ export const onRequest: PagesFunction<Env> = async (context) => {
   });
 
   // get file
-  // const file_url = `https://api.telegram.org/file/bot${env['TG_Bot_Token']}/${file_path}`;
+  const file_url = `https://api.telegram.org/file/bot${env['TG_Bot_Token']}/${file_path}`;
   console.log(file_path)
-  const file_url = `https://api.telegram.org/file/bot8162291976:AAGznGhSV8Pj47IfoEGj6UyQJWKtrB191qg/photos/file_5.jpg`;
+  // const file_url = `https://api.telegram.org/file/bot8162291976:AAGznGhSV8Pj47IfoEGj6UyQJWKtrB191qg/photos/file_5.jpg`;
 
   let res_data;
   await axios.get(file_url, {responseType: 'blob'}).then(res => res_data = res.data)
 
-
   return new Response(res_data,
     {
+      headers: {
+        'Accept-Ranges': 'bytes',
+        'Content-Type': 'application/octet-stream',
+      },
       status: 200
     });
 };
