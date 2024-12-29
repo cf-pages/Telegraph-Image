d55d5e9110ea388996f93851e52f134f9e76bccf
txwdzxq <txwdzxq@sina.com>
2024-12-30 00:59:59 +0800

    is

--- functions/file/[path].ts
+++ functions/file/[path].ts
@@ -7,14 +7,16 @@ interface Env {
 export const onRequest: PagesFunction<Env> = async (context) => {
   const {request, env,} = context;
   // get file path json
-  const url:URL = new URL(request.url);
+  const url = new URL(request.url);
   const path = url.pathname.split(".")[0].split("/")[2];
+  console.log('path', path);
   const get_path_url = `https://api.telegram.org/bot${env['TG_Bot_Token']}/getFile?file_id=${path}`;
-  console.log(path);
+  console.log('get_path_url', get_path_url);
   // const url = `https://api.telegram.org/bot8162291976:AAGznGhSV8Pj47IfoEGj6UyQJWKtrB191qg/getFile?file_id=AgACAgUAAyEGAASQv4IqAAMHZ29c4WtA7PjpKtLaCcKvVEDAEjAAAkPBMRssGXhXRD_Nin8nX64BAAMCAAN5AAM2BA`;
   let file_path;
   axios.get(get_path_url).then(
     (res: AxiosResponse) => {
+      console.log('res.data',res.data);
       file_path = res.data.file_path;
     }).catch(() => {
     file_path = 'photos/file_5.jpg'
@@ -22,7 +24,7 @@ export const onRequest: PagesFunction<Env> = async (context) => {
 
   // get file
   const file_url = `https://api.telegram.org/file/bot${env['TG_Bot_Token']}/${file_path}`;
-  console.log(file_path)
+  console.log('file_path',file_path)
   // const file_url = `https://api.telegram.org/file/bot8162291976:AAGznGhSV8Pj47IfoEGj6UyQJWKtrB191qg/photos/file_5.jpg`;
 
   let res_data;
